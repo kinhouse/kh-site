5b2ad10160a08d506965d9de6712947308b181fb
Gabe Rosenhouse <rosenhouse@gmail.com>
2015-01-31 12:54:22 -0800

    Refactor main

--- main.go
+++ main.go
@@ -20,6 +20,56 @@ func loadPage(title string) template.HTML {
 	return template.HTML(body)
 }
 
+type PersistInterface interface {
+	GetAllRSVPs() ([]persist.Rsvp, error)
+	InsertNewRSVP(persist.Rsvp) (int64, error)
+}
+
+type Config struct {
+	data         PersistInterface
+	pageTemplate *template.Template
+	pages        map[string]template.HTML
+	assets       []string
+}
+
+func BuildRouterGroup(config Config) *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/api/v0/rsvps", func(c *gin.Context) {
+		rsvps, err := config.data.GetAllRSVPs()
+		if err != nil {
+			c.Fail(500, err)
+		}
+		c.JSON(200, rsvps)
+	})
+
+	r.POST("/api/v0/rsvps", func(c *gin.Context) {
+		var rsvp persist.Rsvp
+		if c.Bind(&rsvp) {
+			fmt.Printf("got rsvp post: %+v\n", rsvp)
+			id, err := config.data.InsertNewRSVP(rsvp)
+			if err != nil {
+				c.Fail(500, err)
+			}
+			c.JSON(201, gin.H{"id": id})
+		}
+	})
+
+	for route, body := range config.pages {
+		pageBody := body
+		r.GET("/"+route, func(c *gin.Context) {
+			config.pageTemplate.Execute(c.Writer, pageBody)
+		})
+	}
+
+	for _, filename := range config.assets {
+		asset := filename
+		r.GET("/"+asset, func(c *gin.Context) { c.File("assets/" + asset) })
+	}
+
+	return r
+}
+
 func main() {
 
 	data, err := persist.NewPersist()
@@ -35,47 +85,17 @@ func main() {
 	locationPage := loadPage("event")
 	homePage := loadPage("home")
 
-	r := gin.Default()
-
-	r.GET("/api/v0/rsvps", func(c *gin.Context) {
-		rsvps, err := data.GetAllRSVPs()
-		if err != nil {
-			c.Fail(500, err)
-		}
-		c.JSON(200, rsvps)
-	})
-
-	r.POST("/api/v0/rsvps", func(c *gin.Context) {
-		var rsvp persist.Rsvp
-		if c.Bind(&rsvp) {
-			fmt.Printf("got rsvp post: %+v\n", rsvp)
-			id, err := data.InsertNewRSVP(rsvp)
-			if err != nil {
-				c.Fail(500, err)
-			}
-			c.JSON(201, gin.H{"id": id})
-		}
-	})
-
-	r.GET("/event", func(c *gin.Context) {
-		pageTemplate.Execute(c.Writer, locationPage)
-	})
-
-	r.GET("/", func(c *gin.Context) {
-		pageTemplate.Execute(c.Writer, homePage)
-	})
-
-	r.GET("/rsvp", func(c *gin.Context) {
-		pageTemplate.Execute(c.Writer, template.HTML("Coming soon..."))
-	})
-
-	staticAssets := []string{"main.css", "map.png", "header.png", "favicon.png"}
-	for _, filename := range staticAssets {
-		asset := filename
-		r.GET("/"+asset, func(c *gin.Context) {
-			c.File("assets/" + asset)
-		})
+	config := Config{
+		data:         data,
+		pageTemplate: pageTemplate,
+		pages: map[string]template.HTML{
+			"":      homePage,
+			"rsvp":  template.HTML("Coming soon..."),
+			"event": locationPage,
+		},
+		assets: []string{"main.css", "map.png", "header.png", "favicon.png"},
 	}
 
-	r.Run(":" + os.Getenv("PORT"))
+	routerGroup := BuildRouterGroup(config)
+	routerGroup.Run(":" + os.Getenv("PORT"))
 }
