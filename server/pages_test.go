91dd18e33c1224f5bc6cab6f633cb936c2194932
Gabe Rosenhouse <rosenhouse@gmail.com>
2015-02-08 12:40:18 -0800

    improve testing infrastructure

---
+++ server/pages_test.go
@@ -0,0 +1,12 @@
+package server_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Pages", func() {
+	It("works", func() {
+		Expect(true).To(BeTrue())
+	})
+})
